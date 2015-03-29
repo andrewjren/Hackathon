65ba2c388f87d0e3c6e79870c55e0dd4209dbafd
andrewjren <andrewjren@gmail.com>
2015-03-28 23:53:34 -0400

    for david

---
+++ flex/flex.ino
@@ -0,0 +1,38 @@
+int sensor = A0;
+int sensorvalue = 0;
+const unsigned int R = 9;
+const unsigned int G = 10;
+const unsigned int B = 11;
+int red;
+int green;
+int blue;
+
+
+void setup()
+{
+  pinMode(R, OUTPUT);
+  pinMode(G, OUTPUT);
+  pinMode(B, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  sensorvalue = analogRead(sensor);
+  byte note = map(sensorvalue, 100, 240, 0, 100);
+  
+  color(note - 100, note + 50, note);
+  delay(0050);
+  Serial.print(note, DEC);
+  Serial.print("\n");
+//  Serial.print(sensorvalue, DEC);
+//  Serial.print("\n");
+}
+
+void color(unsigned int red, unsigned int green, unsigned int blue)
+{
+  analogWrite(R, red);
+  analogWrite(G, green);
+  analogWrite(B, blue);
+}
